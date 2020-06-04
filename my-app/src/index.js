54df09ed61ac11293f897aefd436866a1b4126db
miyokohirao <uttttti@gmail.com>
2020-06-04 16:22:17 +0900

    add logger

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { GameContainer } from './containers';
 import { gameReducer } from "./mutations";
+import { logger } from "./middlewares";
 import "./index.css";
 
 
 const app = combineReducers({ game: gameReducer });
-const store = createStore(app);
+const store = createStore(app, applyMiddleware(logger));
 ReactDOM.render(
   <Provider store={store}>
     <GameContainer />
