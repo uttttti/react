ae0305d88aec18672c427279d973414e4095aa9f
miyokohirao <uttttti@gmail.com>
2020-06-17 20:39:54 +0900

    axios でリクエストをし、得た情報を DOM に反映させる

--- udemy/api-app/src/index.js
+++ udemy/api-app/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
+
+const search = 'cat';
+const key = '0aDuUjQfzHxXgqBp27lk7kN66IEPfRiN';
+const limit = 3;
+const url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${key}&limit=${limit}`;
+
+
+axios.get(url).then(res => {
+  console.log(res.data);
+  const data = res.data.data;
+  const imgUrl = data[0].images.downsized.url;
+  console.log(imgUrl);
+  const img = document.createElement('img');
+  img.src = imgUrl;
+  document.body.appendChild(img);
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
